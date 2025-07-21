f0ec935580e4411559ae0c4b130cdf67a1de4253
Evgeny Vinnik <evgenyvinnik@ya.ru>
2025-07-20 22:17:47 -0700

    switch to vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 1998,
+    open: true
+  }
+});
